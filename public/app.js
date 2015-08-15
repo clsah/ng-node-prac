9047bffd362e602856a9081b2856b89b7e378ff7
unknown <chotelalsah7@gmail.com>
2015-08-15 16:06:28 +0530

    add files

---
+++ public/app.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+ 
+angular.module('shoppingCart', [
+     'ngRoute',
+    'cart',
+    'checkout'
+    
+]).
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({
+        redirectTo: '/cart'
+    });
+}]);
